152de9396749bbc828db869c31939a4474884460
HANIIF FII SABIILILLAH <hanif_dwiguna@hotmail.com>
2019-10-06 23:48:50 +0700

    Add author info and question creation date in question item

--- app/Question.php
+++ app/Question.php
@@ -17,4 +17,15 @@ class Question extends Model
         $this->attributes['title'] = $value;
         $this->attributes['slug'] = Str::slug($value);
     }
+
+    public function getUrlAttribute()
+    {
+        return route('questions.show', $this->id);
+    }
+
+    public function getCreatedDateAttribute()
+    {
+        // return $this->created_at->format('d/m/Y');
+        return $this->created_at->diffForHumans();
+    }
 }

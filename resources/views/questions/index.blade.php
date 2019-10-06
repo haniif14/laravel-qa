fb1572fc39157da4863d9a51aef9b701038d54a9
HANIIF FII SABIILILLAH <hanif_dwiguna@hotmail.com>
2019-10-07 04:05:20 +0700

    Implement form validation and store the questions

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -14,9 +14,9 @@
                       </a>
                     </div>
                   </div>                
-                </div>
-
+                </div>                                
                 <div class="card-body">
+                   @include('layouts._messages')
                    @foreach ($questions as $question)
                     <div class="media">
                       <div class="d-flex flex-column counters">
